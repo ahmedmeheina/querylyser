b52844f0993f02cf5584c2ab387c586ca19b6f0d
ahmedmeheina <ahmedmeheina@gmail.com>
2022-07-24 20:10:13 +0200

    log queries

--- src/Listeners/LogQuery.php
+++ src/Listeners/LogQuery.php
@@ -3,11 +3,15 @@
 namespace AMeheina\Querylyser\Listeners;
 
 use Illuminate\Database\Events\QueryExecuted;
+use AMeheina\Querylyser\Models\LoggedQuery;
 
 class LogQuery
 {
     public function handle(QueryExecuted $query)
     {
-        dd($query->sql);
+        $loggedQuery = new LoggedQuery();
+        $loggedQuery->sql = $query->sql;
+        $loggedQuery->time = $query->time;
+        $loggedQuery->save();
     }
 }
